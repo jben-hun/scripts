8d7458b7a6c103a8182af03821de17657ae5f65d
JBen <jben-hun@users.noreply.github.com>
2020-02-18 11:13:27 +0100

    ...

--- template.py
+++ template.py
@@ -1,14 +1,16 @@
 #! /usr/bin/python3
 
-import argparse
+import argparse, json, os, shutil
+from os import sys,path
+from pprint import pprint
 from sys import exit
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("file")
+    # parser.add_argument("file")
     args = parser.parse_args()
 
-    print(args.file)
+
 
 if __name__ == '__main__':
     main()
