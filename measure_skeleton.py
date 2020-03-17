b566e6b037bb7ff5038bd48f4b0b9bca5d6c6a87
Zsolt Santa <zssanta@ultinous.com>
2020-03-17 13:57:03 +0100

    Implement skeleton based evaluator

---
+++ measure_skeleton.py
@@ -0,0 +1,99 @@
+#! /usr/bin/env python3
+
+import argparse
+import json
+import numpy as np
+import pandas as pd
+
+import detector_benchmark.util as util
+import detector_benchmark.pair as pair
+
+
+def open_dataset(filename):
+    with open(filename, "rb") as fp:
+        return json.load(fp)
+
+
+def create_arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("gt_path")
+    parser.add_argument("query_path")
+    parser.add_argument("--score_threshold", type=float, default=0.5)
+    parser.add_argument("--skeleton", default=False, action="store_true")
+    parser.add_argument("--output_name", default="measurements.csv")
+    return parser
+
+
+def convert_skeleton_to_bb_format(skeleton_file):
+    boxes = []
+    scores = []
+    for skel in skeleton_file["skeletons"]:
+        points = np.array([(pt["x"], pt["y"]) for pt in skel])
+        bb = np.hstack((points.min(axis=0), points.max(axis=0)))
+        boxes.append(list(bb))
+        scores.append(np.array([pt["confidence"] for pt in skel]).mean())
+    return {
+        "image_path": skeleton_file["filename"],
+        "boxes": boxes,
+        "scores": scores,
+        "skeletons": skeleton_file["skeletons"],
+    }
+
+
+def skeleton_cost(detection, detection_type, annotation_box, annotation_type):
+    assert detection_type == "skeleton" and annotation_type == "body"
+    points = np.array([(pt["x"], pt["y"]) for pt in detection])
+    inside = (points[:, 0] >= annotation_box[0]) \
+        & (points[:, 0] <= annotation_box[2]) \
+        & (points[:, 1] >= annotation_box[1]) \
+        & (points[:, 1] <= annotation_box[3])
+    return -float(inside.sum()) / len(detection)
+
+
+def extract_detections_skeletons(annotationList, annotationType, detectionList, detectionType, scaleRange, scoreThreshold,
+                                 mask=False):
+    gtCount = 0
+    detections = []
+    for index in range(len(annotationList)):
+        annotationLabels = annotationList[index]["labels"]
+        annotationBoxes = annotationList[index]["boxes"]
+        detectionScores = detectionList[index]["scores"]
+        detectionSkeletons = detectionList[index]["skeletons"]
+
+        assert annotationList[index]["image_path"] == detectionList[index]["image_path"], \
+            "{} != {}".format(annotationList[index]["image_path"], detectionList[index]["image_path"])
+
+        filteredAnnotationBoxes = util.heightFilter(annotationBoxes, minHeight=scaleRange[0], maxHeight=scaleRange[1],
+                                                    labels=annotationLabels if mask else None)
+        positiveCount = len(filteredAnnotationBoxes)
+        gtCount += positiveCount
+
+        detection = util.max_pair(detectionSkeletons, detectionScores, detectionType, annotationBoxes, annotationLabels,
+                                  annotationType, -scoreThreshold, minHeight=scaleRange[0], maxHeight=scaleRange[1],
+                                  mask=mask, cost_fun=skeleton_cost)
+        detections.extend(detection)
+    return detections, gtCount
+
+
+def main():
+    parser = create_arg_parser()
+    args = parser.parse_args()
+
+    gt_dataset = util.readList(args.gt_path, count=1, detection=False)
+    query_dataset = open_dataset(args.query_path)
+    query_filename_to_ind = {f["filename"]: ind for ind, f in enumerate(query_dataset)}
+
+    query_bbox_dataset = [convert_skeleton_to_bb_format(query_dataset[query_filename_to_ind[gt_file["image_path"]]])
+                          for gt_file in gt_dataset]
+    if args.skeleton:
+        stats = pair.eval_scales(gt_dataset, "body", query_bbox_dataset, "skeleton", args.score_threshold,
+                                 testNamePrefix="test1", extract_detections_fun=extract_detections_skeletons)["all"]
+    else:
+        stats = pair.eval_scales(gt_dataset, "body", query_bbox_dataset, "body", args.score_threshold,
+                                 testNamePrefix="test1")["all"]
+
+    pd.DataFrame(stats).to_csv(args.output_name, index=False, header=False, sep="\t")
+
+
+if __name__ == "__main__":
+    main()
